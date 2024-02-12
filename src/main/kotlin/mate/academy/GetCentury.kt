9374a2788da6430ea758426450c5ffe7dd35cbb7
Svyatoslav Tsytsyura <svatik.bos@gmail.com>
2024-02-12 16:06:56 +0200

    done

--- src/main/kotlin/mate/academy/GetCentury.kt
+++ src/main/kotlin/mate/academy/GetCentury.kt
@@ -2,6 +2,8 @@ package mate.academy
 
 import kotlin.math.ceil
 
+const val HUNDRED = 100
+
 fun getCentury(year: Int) : Int {
-    return ceil(year.toDouble() / 100).toInt()
+    return ceil(year.toDouble() / HUNDRED).toInt()
 }
