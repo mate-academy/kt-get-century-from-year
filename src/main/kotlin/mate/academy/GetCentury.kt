c6a1630348a28ced4f1396716a5191075ffbc745
Anton Dudar <dudar.anton4uk@gmail.com>
2024-02-15 14:09:17 +0200

    solution

--- src/main/kotlin/mate/academy/GetCentury.kt
+++ src/main/kotlin/mate/academy/GetCentury.kt
@@ -1,5 +1,12 @@
 package mate.academy
+const val YEAR_DIVISOR = 100
+const val ZERO_REMAINDER = 0
+const val CENTURY_ADDITION = 1
 
 fun getCentury(year: Int) : Int {
-    return 0
+    return if (year % YEAR_DIVISOR == ZERO_REMAINDER) {
+        year / YEAR_DIVISOR
+    } else {
+        year / YEAR_DIVISOR + CENTURY_ADDITION
+    }
 }
