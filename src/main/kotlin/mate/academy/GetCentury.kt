637a51cc4dd970f284de9f7443691b5756f3e330
Inna Diachkina <innadyachkina@gmail.com>
2024-02-18 19:05:30 +0200

    add const

--- src/main/kotlin/mate/academy/GetCentury.kt
+++ src/main/kotlin/mate/academy/GetCentury.kt
@@ -1,11 +1,14 @@
 package mate.academy
 
+const val DIVISOR = 10
+const val CENTURY_MULTIPLIER = 100
+
 fun getCentury(year: Int) : Int {
-    val result : Int
-    if (year % 10 == 0) {
-        result = year / 100
+    var result: Int
+    if (year % DIVISOR == 0) {
+        result = year / CENTURY_MULTIPLIER
     } else {
-        result = year / 100 + 1
+        result = year / CENTURY_MULTIPLIER + 1
     }
     return result
 }
