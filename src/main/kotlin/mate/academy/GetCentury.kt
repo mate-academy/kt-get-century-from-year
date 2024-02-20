778e1e59d7f7181cc9a1da13fdb0e3eaa03abf1a
Mykhailo Dymusha <mykhailo.dymusha@gmail.com>
2024-02-20 15:23:31 +0200

    Made constants for magic numbers.

--- src/main/kotlin/mate/academy/GetCentury.kt
+++ src/main/kotlin/mate/academy/GetCentury.kt
@@ -1,5 +1,11 @@
 package mate.academy
 
+private const val CENTURY_DEVIDER = 100
+
+private const val NEXT_CENTURY = 1
+
+private const val ZERO = 0
+
 fun getCentury(year: Int) : Int {
-    return if (year % 100 == 0) year / 100 else year / 100 + 1
+    return if (year % CENTURY_DEVIDER == ZERO) year / CENTURY_DEVIDER else year / CENTURY_DEVIDER + NEXT_CENTURY
 }
