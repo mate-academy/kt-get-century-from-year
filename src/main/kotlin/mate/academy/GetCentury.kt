df8e119819413433fa280eb10ce5091191561df2
Taras Machyta <taras.machyta.sa@gmail.com>
2024-02-22 18:28:14 +0200

    Solution

--- src/main/kotlin/mate/academy/GetCentury.kt
+++ src/main/kotlin/mate/academy/GetCentury.kt
@@ -1,5 +1,9 @@
 package mate.academy
 
+const val TYPE = 1
+const val NUMBER = 100
+
 fun getCentury(year: Int) : Int {
-    return 0
+    val century: Int = (year - TYPE) / NUMBER + TYPE
+    return century
 }
