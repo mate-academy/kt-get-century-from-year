d452b863febb0bbbd08a999e528cbac796a80c97
Denys Okhrymovych <denys.ohrymovych.2015@gmail.com>
2024-03-11 13:22:51 +0200

    return previous version of GetCentury.kt

--- src/main/kotlin/mate/academy/GetCentury.kt
+++ src/main/kotlin/mate/academy/GetCentury.kt
@@ -1,17 +1,5 @@
 package mate.academy
 
-fun main() {
-    println(getCentury(1705))
-    println(getCentury(1900))
-    println(getCentury(1601))
-    println(getCentury(2000))
-}
-
 fun getCentury(year: Int) : Int {
-    var century = year / 100
-
-    if (year % 100 > 0) {
-        century++
-    }
-    return century
+    return 0
 }
