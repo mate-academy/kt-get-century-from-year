f288739981321dd76e67fc0d75e22b8e0d43e116
dmytro <dmytro.cherepov.ukr@gmail.com>
2024-03-12 12:32:59 +0200

    res

--- src/main/kotlin/mate/academy/GetCentury.kt
+++ src/main/kotlin/mate/academy/GetCentury.kt
@@ -1,5 +1,12 @@
 package mate.academy
 
 fun getCentury(year: Int) : Int {
+    if (year / 100 != 0) {
+        if (year % 10 != 0) {
+            return year / 100 + 1
+        }
+    } else {
+        return year
+    }
     return 0
 }
