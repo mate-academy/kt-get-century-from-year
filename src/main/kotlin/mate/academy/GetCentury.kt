b9b1f1b4bf32d4793d01ef551c39919481d09243
PavloSliepchenko <sliepchenkopavlo@gmail.com>
2024-04-10 16:00:52 -0500

    Added the task solution

--- src/main/kotlin/mate/academy/GetCentury.kt
+++ src/main/kotlin/mate/academy/GetCentury.kt
@@ -1,5 +1,24 @@
 package mate.academy
 
+const val NUMBER_OF_CHARS_TO_DROP = 2
+const val ZERO = 0
+
 fun getCentury(year: Int) : Int {
-    return 0
+    val dateCharArray = "0000".toCharArray()
+
+    for ((index, value) in year.toString().reversed().toCharArray().withIndex()) {
+        dateCharArray[index] = value
+    }
+
+    val localYear = dateCharArray
+        .map { it.toString() }
+        .reduce { result, e -> "$result$e" }
+        .reversed()
+
+    var century = localYear.dropLast(NUMBER_OF_CHARS_TO_DROP).toInt()
+    val decade = localYear.drop(NUMBER_OF_CHARS_TO_DROP).toInt()
+    if (decade > ZERO) {
+        return ++century
+    }
+    return century
 }
