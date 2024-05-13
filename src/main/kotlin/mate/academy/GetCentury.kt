3acff71736d77fefe6d1465870011f2cc404c537
Danylo Borysenko <borysenkodanyl@gmail.com>
2024-05-13 11:58:08 +0300

    first commit

--- src/main/kotlin/mate/academy/GetCentury.kt
+++ src/main/kotlin/mate/academy/GetCentury.kt
@@ -1,5 +1,10 @@
 package mate.academy
 
 fun getCentury(year: Int) : Int {
-    return 0
+    val remindResult = year % 100
+    return if (remindResult == 0) {
+        year / 100
+    } else {
+        (year / 100) + 1
+    }
 }
