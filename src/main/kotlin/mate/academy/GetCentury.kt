162ad17eaf3e14a8e55c310a0e18b81ba1b812cc
Solomia Bobyak <mia.bobyak@gmail.com>
2024-06-07 22:51:59 +0300

    fixed mistake with magic numbers

--- src/main/kotlin/mate/academy/GetCentury.kt
+++ src/main/kotlin/mate/academy/GetCentury.kt
@@ -1,8 +1,12 @@
 package mate.academy
 
-fun getCentury(year: Int) : Int {
-    if (year % 100 == 0) {
-        return year / 100
+const val CENTURY_LENGTH = 100
+const val ONE_YEAR = 1
+const val ZERO = 0
+
+fun getCentury(year: Int): Int {
+    if (year % CENTURY_LENGTH == ZERO) {
+        return year / CENTURY_LENGTH
     }
-    return year / 100 + 1
+    return year / CENTURY_LENGTH + ONE_YEAR
 }
