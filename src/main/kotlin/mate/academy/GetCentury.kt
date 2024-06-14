70de88d42e77b9828b3fa01f4f8d7b462fd3703e
oleksii.b <oleksii.b@dev.tagsoft.co>
2024-06-14 11:32:42 +0300

    refactored get century task

--- src/main/kotlin/mate/academy/GetCentury.kt
+++ src/main/kotlin/mate/academy/GetCentury.kt
@@ -1,5 +1,9 @@
 package mate.academy
 
+private const val YEARS_PER_CENTURY = 100
+private const val ZERO_DIVIDE_REST = 0
+private const val ONE_DIVIDE_REST = 1
+
 fun getCentury(year: Int): Int {
-    return year / 100 + if (year % 100 != 0) 1 else 0
+    return year / YEARS_PER_CENTURY + if (year % YEARS_PER_CENTURY != ZERO_DIVIDE_REST) ONE_DIVIDE_REST else ZERO_DIVIDE_REST
 }
