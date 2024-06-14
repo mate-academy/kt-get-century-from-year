fc53e5e12c5909d14bed911dbc882007dea07169
oleksii.b <oleksii.b@dev.tagsoft.co>
2024-06-14 11:36:16 +0300

    refactored string length in get century task

--- src/main/kotlin/mate/academy/GetCentury.kt
+++ src/main/kotlin/mate/academy/GetCentury.kt
@@ -5,5 +5,6 @@ private const val ZERO_DIVIDE_REST = 0
 private const val ONE_DIVIDE_REST = 1
 
 fun getCentury(year: Int): Int {
-    return year / YEARS_PER_CENTURY + if (year % YEARS_PER_CENTURY != ZERO_DIVIDE_REST) ONE_DIVIDE_REST else ZERO_DIVIDE_REST
+    return year / YEARS_PER_CENTURY +
+            if (year % YEARS_PER_CENTURY != ZERO_DIVIDE_REST) ONE_DIVIDE_REST else ZERO_DIVIDE_REST
 }
