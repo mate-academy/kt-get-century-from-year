02b6ebcfe0b9e8a087384ef210de698d605ae2cd
serhiibiria <serhiiburia@gmail.com>
2024-06-19 17:45:16 +0300

    added solution

--- src/main/kotlin/mate/academy/GetCentury.kt
+++ src/main/kotlin/mate/academy/GetCentury.kt
@@ -1,5 +1,10 @@
 package mate.academy
 
 fun getCentury(year: Int) : Int {
-    return 0
+    val range = 1..year.toString().take(2).toInt() * 100
+    val century: Int = when {
+        year in range -> year.toString().take(2).toInt()
+        else -> year.toString().take(2).toInt() + 1
+    }
+    return century
 }
