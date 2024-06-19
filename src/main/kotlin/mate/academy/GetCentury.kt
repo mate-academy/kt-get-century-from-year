faca97c27ba5bd273cf952c0e565515ca010fa8f
serhiibiria <serhiiburia@gmail.com>
2024-06-19 18:27:22 +0300

    update solution

--- src/main/kotlin/mate/academy/GetCentury.kt
+++ src/main/kotlin/mate/academy/GetCentury.kt
@@ -1,14 +1,16 @@
 package mate.academy
 
+const val YEAR_MULTIPLAER = 100
+const val APP_YEAR = 1
+val RANGE_TO_HUNDREDS = APP_YEAR..YEAR_MULTIPLAER
+
 fun getCentury(year: Int) : Int {
-    val yearMultiplier = 100
-    val rangeToHundreds = 1..100
-    val rangeAfterHundreds = 1..year.toString().take(2).toInt() * yearMultiplier
-    val appYear = 1
+    val rangeAfterHundreds = 1..year.toString().take(2).toInt() * YEAR_MULTIPLAER
+
     val century: Int = when {
-        year in rangeToHundreds -> year.toString().take(1).toInt()
+        year in RANGE_TO_HUNDREDS -> year.toString().take(1).toInt()
         year in rangeAfterHundreds -> year.toString().take(2).toInt()
-        else -> year.toString().take(2).toInt() + appYear
+        else -> year.toString().take(2).toInt() + APP_YEAR
     }
     return century
 }
