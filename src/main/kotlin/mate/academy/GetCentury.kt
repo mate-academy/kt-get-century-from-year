0fa476881209248081108cc2d2ff052733453b0f
Oksana Miazina <oksana.myazina@gmail.com>
2024-06-25 16:37:04 +0300

    style: add constants

--- src/main/kotlin/mate/academy/GetCentury.kt
+++ src/main/kotlin/mate/academy/GetCentury.kt
@@ -1,5 +1,8 @@
 package mate.academy
 
+private const val YEARS_IN_CENTURY = 100
+private const val CENTURY_START = 1
+
 fun getCentury(year: Int) : Int {
-    return (year - 1) / 100 + 1
+    return (year - CENTURY_START) / YEARS_IN_CENTURY + CENTURY_START
 }
