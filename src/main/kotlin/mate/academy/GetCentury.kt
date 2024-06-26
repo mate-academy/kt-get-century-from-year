a75c24e93edb0f6b5f7681c24837278db4cc56ac
sviatoslavtataryn <www.t.svyatoslav55@gmail.com>
2024-06-26 15:13:27 +0200

    fixed magic numbers

--- src/main/kotlin/mate/academy/GetCentury.kt
+++ src/main/kotlin/mate/academy/GetCentury.kt
@@ -1,9 +1,13 @@
 package mate.academy
 
+const val CENTURY_DIVISOR = 100
+const val CENTURY_OFFSET = 99
+const val TEST_VALUE = 1997
+
 fun main() {
-    println(getCentury(1997))
+    println(getCentury(TEST_VALUE))
 }
 
 fun getCentury(year: Int) : Int {
-    return (year + 99) / 100
+    return (year + CENTURY_OFFSET) / CENTURY_DIVISOR
 }
