7acf56e2c28a06593240e8670c466d779c1c7e78
Viktoriia Ihnatiuk <viktoriia.ihnatiuk@techtellent.io>
2024-08-25 19:03:00 +0300

    magic numbers have been replaced with constants

--- src/main/kotlin/mate/academy/GetCentury.kt
+++ src/main/kotlin/mate/academy/GetCentury.kt
@@ -1,9 +1,12 @@
 package mate.academy
 
+const val YEARS_IN_CENTURY = 100
+const val CENTURY_INCREMENT = 1
+
 fun getCentury(year: Int) : Int {
-    return if (year % 100 == 0) {
-        year / 100
+    return if (year % YEARS_IN_CENTURY == 0) {
+        year / YEARS_IN_CENTURY
     } else {
-        year / 100 + 1
+        year / YEARS_IN_CENTURY + CENTURY_INCREMENT
     }
 }
