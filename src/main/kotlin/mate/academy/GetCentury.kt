a8883eb9addef02cd6c1564fb517b8df01b29af0
ilko-ilya <samilyakilya@gmail.com>
2024-08-30 21:54:46 +0300

    second commit, add constants

--- src/main/kotlin/mate/academy/GetCentury.kt
+++ src/main/kotlin/mate/academy/GetCentury.kt
@@ -1,9 +1,12 @@
 package mate.academy
 
+const val CENTURY_DIVIDER = 100
+const val NEXT_CENTURY_INCREMENT = 1
+
 fun getCentury(year: Int) : Int {
-    return if (year % 100 == 0) {
-        year / 100
+    return if (year % CENTURY_DIVIDER == 0) {
+        year / CENTURY_DIVIDER
     } else {
-        year / 100 + 1
+        year / CENTURY_DIVIDER + NEXT_CENTURY_INCREMENT
     }
 }
