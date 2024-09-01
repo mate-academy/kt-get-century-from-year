9d02959c5eab4e3507aedc33f8906e26da8e525c
0-zagar-0 <git.hub.zagar@gmail.com>
2024-09-01 23:09:36 +0200

    implemented solution

--- src/main/kotlin/mate/academy/GetCentury.kt
+++ src/main/kotlin/mate/academy/GetCentury.kt
@@ -1,5 +1,10 @@
 package mate.academy
 
+const val YEARS_OF_CENTURY = 100
+const val ZERO = 0
+const val ONE_CENTURY = 1
+const val ROUNDING_THRESHOLD = 10
+
 fun getCentury(year: Int) : Int {
-    return 0
+    return if (year % ROUNDING_THRESHOLD != ZERO) year / YEARS_OF_CENTURY + ONE_CENTURY else year / YEARS_OF_CENTURY
 }
