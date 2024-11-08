597693acc84f57b468641797aa3a51e5fff89bc9
Anatolii Nazarov <anatoly_n@ukr.net>
2024-11-08 22:14:44 +0200

    For check

--- src/main/kotlin/mate/academy/GetCentury.kt
+++ src/main/kotlin/mate/academy/GetCentury.kt
@@ -1,5 +1,8 @@
 package mate.academy
 
-fun getCentury(year: Int) : Int {
-    return (year + 99) / 100
+const val CENTURY_DIVIDER = 100
+const val CENTURY_BASE = 1
+
+fun getCentury(year: Int): Int {
+    return (year + CENTURY_DIVIDER - CENTURY_BASE) / CENTURY_DIVIDER
 }
