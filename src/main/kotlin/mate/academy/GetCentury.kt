be136f2d8ada45705bf8171e6c5d239a7f880748
Artem Klishch <artemklishch@gmail.com>
2024-11-20 16:20:46 +0200

    feat: function to calculate century

--- src/main/kotlin/mate/academy/GetCentury.kt
+++ src/main/kotlin/mate/academy/GetCentury.kt
@@ -1,5 +1,11 @@
 package mate.academy
 
-fun getCentury(year: Int) : Int {
-    return 0
+const val CENTURY_COEFFICIENT = 100
+
+fun getCentury(year: Int): Int {
+    if (year % CENTURY_COEFFICIENT == 0) {
+        return year / CENTURY_COEFFICIENT
+    } else {
+        return (year / CENTURY_COEFFICIENT) + 1
+    }
 }
