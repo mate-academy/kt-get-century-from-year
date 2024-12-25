8c5c05f1669cf297f2626cf3a4c490ec34cc9f27
Vitaliy <vitaliyfilimonov3@gmail.com>
2024-12-25 15:29:38 +0200

    solution

--- src/main/kotlin/mate/academy/GetCentury.kt
+++ src/main/kotlin/mate/academy/GetCentury.kt
@@ -1,6 +1,7 @@
 package mate.academy
+const val CENTURY = 100
 
 fun getCentury(year: Int) : Int {
-    var i = year / 100
-    return if (year % 100 == 0) i else ++i
+    var i = year / CENTURY
+    return if (year % CENTURY == 0) i else ++i
 }
