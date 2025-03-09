f16a990a8ffe48f2b013f23577d381fa6990fbfa
Kateryna Mykhailovska <112807544+kateryna-mykh@users.noreply.github.com>
2025-03-09 17:01:33 +0200

    Update GetCentury.kt

--- src/main/kotlin/mate/academy/GetCentury.kt
+++ src/main/kotlin/mate/academy/GetCentury.kt
@@ -1,5 +1,11 @@
 package mate.academy
 
+const val CENTURY = 100
+
 fun getCentury(year: Int) : Int {
-    return 0
+    var result = year / 100;
+    if (year % CENTURY > 0) {
+        ++result;
+    }
+    return  result
 }
