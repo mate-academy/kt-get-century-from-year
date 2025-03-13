75e7c7df732ed19d0653fafa2f8bb61d60d4f3f4
Roman Voynahiy <romanvoynahiy@gmail.com>
2025-03-13 15:42:55 +0200

    Implemented getCentury() function to calculate the century for a given year.

--- src/main/kotlin/mate/academy/GetCentury.kt
+++ src/main/kotlin/mate/academy/GetCentury.kt
@@ -1,5 +1,13 @@
 package mate.academy
 
+const val CENTURY_DIVISOR = 100
+const val CENTURY_OFFSET = 1
+const val NO_REMAINDER = 0
+
 fun getCentury(year: Int) : Int {
-    return 0
+    if (year % CENTURY_DIVISOR == NO_REMAINDER) {
+        return year / CENTURY_DIVISOR
+    } else {
+        return (year / CENTURY_DIVISOR) + CENTURY_OFFSET
+    }
 }
