f42abe3db8c0d198884fc0d9ac5e946e7ea8b59f
aksoli666 <olexsandradorofeieva@gmail.com>
2025-03-13 11:12:54 +0100

    2.0 Add constant. Build successful

--- src/main/kotlin/mate/academy/GetCentury.kt
+++ src/main/kotlin/mate/academy/GetCentury.kt
@@ -1,5 +1,9 @@
 package mate.academy
 
+const val HUNDRED = 100
+const val ONE = 1
+const val ZERO = 0
+
 fun getCentury(year: Int) : Int {
-    return if (year % 100 == 0) year / 100 else (year / 100) + 1
+    return if (year % HUNDRED == ZERO) year / HUNDRED else (year / HUNDRED) + ONE
 }
