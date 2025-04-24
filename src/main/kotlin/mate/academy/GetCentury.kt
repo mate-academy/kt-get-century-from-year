a011d6471b5a9e11c265a0272c19d097695aa605
dantesworld <dantesworld2@gmail.com>
2025-04-24 12:18:11 +0300

    first attempt

--- src/main/kotlin/mate/academy/GetCentury.kt
+++ src/main/kotlin/mate/academy/GetCentury.kt
@@ -1,5 +1,10 @@
 package mate.academy
+private const val CENTURY = 100
 
 fun getCentury(year: Int) : Int {
-    return 0
+    if (year % CENTURY != 0) {
+        return (year / CENTURY) + 1
+    } else {
+        return year / CENTURY
+    }
 }
