30983d711fd1fd3062e1d16f7c09a98bcbc066ec
VArabchuk <arabon3@gmail.com>
2025-05-09 17:05:52 +0300

    Initial commit1

--- src/main/kotlin/mate/academy/GetCentury.kt
+++ src/main/kotlin/mate/academy/GetCentury.kt
@@ -1,8 +1,10 @@
 package mate.academy
 
+private const val CENTURY_LENGTH = 100
+
 fun getCentury(year: Int) : Int {
-    val temp = year / 100
-    val diff = year % 100
+    val temp = year / CENTURY_LENGTH
+    val diff = year % CENTURY_LENGTH
     return if (diff == 0) {
         temp
     } else {
