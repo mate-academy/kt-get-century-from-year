577751e6229cb34c2fe9f8a010fef5e7e2b44f56
Oleksandr Kalenyk <carbonmail88@gmail.com>
2025-05-12 14:11:08 +0300

    kt century solution

--- src/main/kotlin/mate/academy/GetCentury.kt
+++ src/main/kotlin/mate/academy/GetCentury.kt
@@ -1,8 +1,12 @@
 package mate.academy
 
+const val REMAIN = 100
+const val ZERO = 0
+const val ONE = 1
+
 fun getCentury(year: Int) : Int {
-    if (year % 100 != 0)
-        return (year / 100) + 1
+    if (year % REMAIN != ZERO)
+        return (year / REMAIN) + ONE
     else
-        return year / 100
+        return year / REMAIN
 }
