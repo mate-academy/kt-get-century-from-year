f45d6e8448029be33af8a218f253aa124cc7eac5
Bakhmet Ivan <bahmetvanya1290@gmail.com>
2025-05-15 20:20:09 +0300

    Fixed issue with exception throwing

--- src/main/kotlin/mate/academy/GetCentury.kt
+++ src/main/kotlin/mate/academy/GetCentury.kt
@@ -2,6 +2,6 @@ package mate.academy
 
 const val CENTURY = 100
 fun getCentury(year: Int) : Int {
-    if(year < 0) throw IllegalArgumentException("Year cannot be negative")
+    require(year >= 0) { "Year cannot be negative" }
     return if(year % CENTURY == 0) year / CENTURY else year / CENTURY + 1
 }
