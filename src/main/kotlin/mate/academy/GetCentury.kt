cf8c7788a1128214db97f7b9160a7750459b1024
Yaroslav Radevych-Vynnytskyi <yaroslav.radevych.vynnytskyi@teamvoy.com>
2025-05-18 15:32:48 +0300

    implement getCentury()

--- src/main/kotlin/mate/academy/GetCentury.kt
+++ src/main/kotlin/mate/academy/GetCentury.kt
@@ -1,5 +1,11 @@
 package mate.academy
 
+const val HUNDRED = 100
+
 fun getCentury(year: Int) : Int {
-    return 0
+    return if (year % HUNDRED == 0) {
+        year / HUNDRED
+    } else {
+        year / HUNDRED + 1
+    }
 }
